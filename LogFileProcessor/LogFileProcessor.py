37d8c6d2020aebbc237d7e7e2bb3c06a83a6670f
Richardruiming <rrhuang2@illinois.edu>
2025-04-26 17:14:36 -0500

    completed

--- LogFileProcessor/LogFileProcessor.py
+++ LogFileProcessor/LogFileProcessor.py
@@ -1,10 +1,11 @@
 from datetime import date
-file_path = 'logininfo.log'
+file_path = 'SampleLogFile.log'
 COUNT = 0
 with open(file_path, 'r') as file:
     lines = file.readlines()
-    if lines.contains(date.today()):
-        if lines.contains('ERROR'):
+    for line in lines:
+        print(line)
+        if str(date.today()) in line and 'ERROR' in line:
             COUNT += 1
 ErrorFile = 'error.txt'
 with open(ErrorFile, 'a') as file:
---
+++ LogFileProcessor/bash-Command
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd '/mnt/c/Users/HP/OneDrive - University of Illinois - Urbana/2025 Cadence Design Systems Intern/LogFileProcessor$'
+
+python3 LogFileProcessor.py
